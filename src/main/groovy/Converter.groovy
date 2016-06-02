96cd86ce64597109bf8ad4c6f56c2a5e249a938a
msomji <maisam.somji@gmail.com>
2016-06-01 23:05:36 -0400

    major key: refactoring

--- src/main/groovy/Converter.groovy
+++ src/main/groovy/Converter.groovy
@@ -1,36 +1,16 @@
 class Converter {
+    def arabicNumbers = [10, 9, 5, 4, 1]
+    def romanNumerals = ["X", "IX", "V", "IV", "I"]
     def result = []
 
     String toRomanNumeral(int integer) {
-        while (integer > 0){
-            if(integer >= 5){
-                integer = incrementBy5(integer)
-            }else if(integer >=4){
-                integer = incrementBy4(integer)
-            }else {
-                integer = incrementBy1(integer)
+
+        arabicNumbers.eachWithIndex { number, index ->
+            while (number <= integer) {
+                result << romanNumerals[index]
+                integer = integer - number
             }
         }
-
         return result.join()
     }
-
-    private int incrementBy5(int integer) {
-        result << "V"
-        integer = integer - 5
-        integer
-    }
-
-    private int incrementBy4(int integer) {
-        result << "IV"
-        integer = integer - 4
-        integer
-    }
-
-    private int incrementBy1(int integer) {
-        result << 'I'
-        integer = integer - 1
-        integer
-    }
-    
 }
