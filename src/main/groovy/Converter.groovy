88a2d7ff495331aab8a4ca24c5f807f047689268
msomji <maisam.somji@gmail.com>
2016-06-01 22:02:50 -0400

    converting all numbers till 8 to roman numberals

--- src/main/groovy/Converter.groovy
+++ src/main/groovy/Converter.groovy
@@ -1,11 +1,36 @@
 class Converter {
-    String toRomanNumeral(int integer) {
-        def result = []
+    def result = []
 
-        for (integer; integer > 0; --integer) {
-            result << 'I'
+    String toRomanNumeral(int integer) {
+        while (integer > 0){
+            if(integer >= 5){
+                integer = incrementBy5(integer)
+            }else if(integer >=4){
+                integer = incrementBy4(integer)
+            }else {
+                integer = incrementBy1(integer)
+            }
         }
 
         return result.join()
     }
+
+    private int incrementBy5(int integer) {
+        result << "V"
+        integer = integer - 5
+        integer
+    }
+
+    private int incrementBy4(int integer) {
+        result << "IV"
+        integer = integer - 4
+        integer
+    }
+
+    private int incrementBy1(int integer) {
+        result << 'I'
+        integer = integer - 1
+        integer
+    }
+    
 }
--- src/test/groovy/ArabicToRomanSpec.groovy
+++ src/test/groovy/ArabicToRomanSpec.groovy
@@ -3,23 +3,35 @@ import spock.lang.Specification
 
 class ArabicToRomanSpec extends Specification {
 
-    def arabicToRoman = new Converter()
-
+    def underTest = new Converter()
 
     def "should return I when Input is 1"() {
         expect:
-        arabicToRoman.toRomanNumeral(1) == "I"
+        underTest.toRomanNumeral(1) == "I"
     }
 
     def "should return II when input is 2"() {
         expect:
-        arabicToRoman.toRomanNumeral(2) == "II"
-
+        underTest.toRomanNumeral(2) == "II"
     }
 
     def "should return II when input is 3"() {
         expect:
-        arabicToRoman.toRomanNumeral(3) == "III"
+        underTest.toRomanNumeral(3) == "III"
+    }
 
+    def "should return IV when input is 4"() {
+        expect:
+        underTest.toRomanNumeral(4) == "IV"
+    }
+
+    def "should return V when input is 5"() {
+        expect:
+        underTest.toRomanNumeral(5) == "V"
+    }
+
+    def "should return VIII when input is 8"() {
+        expect:
+        underTest.toRomanNumeral(8) == "VIII"
     }
 }
